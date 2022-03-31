c722e8bb5c5b06b7d9dbfee830a55104c70ba237
Dan Moneypenny <catullus1973@gmail.com>
2022-03-31 06:41:03 -0700

    fix author route

--- app/units/authors/author.routes.js
+++ app/units/authors/author.routes.js
@@ -6,7 +6,7 @@ const { create, read, detail, update, remove } = require('./author.controller');
 
 // routes
 authorRouter.route('/').get(read).post(create);
-authorRouter.route('/').get(detail).put(update).delete(remove);
+authorRouter.route('/:id').get(detail).put(update).delete(remove);
 
 // export
 module.exports = authorRouter;
