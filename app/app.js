b0c434f5f1c9c83ff629f3cf54e25cad5037f884
Dan Moneypenny <catullus1973@gmail.com>
2022-03-30 22:38:47 -0700

    update

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ const app = express();
 /**
  * middleware
  */
- app.use(express.static(path.join(__dirname, 'public')));
+ app.use(express.static(path.join(__dirname, 'build')));
  app.use(express.json());
  app.use(express.urlencoded({ extended: false }));
  app.use(cookieParser());
